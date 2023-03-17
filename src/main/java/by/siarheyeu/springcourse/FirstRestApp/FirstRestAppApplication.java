7d7f5e25332d36075bdd1a1f0d283eb076d039b1
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-03-17 22:12:49 +0300

    Initial commit

---
+++ src/main/java/by/siarheyeu/springcourse/FirstRestApp/FirstRestAppApplication.java
@@ -0,0 +1,13 @@
+package by.siarheyeu.springcourse.FirstRestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestAppApplication.class, args);
+	}
+
+}
