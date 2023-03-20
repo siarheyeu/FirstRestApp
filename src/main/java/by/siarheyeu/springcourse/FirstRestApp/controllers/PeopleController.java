c2992779dedff833ebc1fbd28f619c4dacd5ea6c
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-03-20 20:46:48 +0300

    Method that returns Person by ID

--- src/main/java/by/siarheyeu/springcourse/FirstRestApp/controllers/PeopleController.java
+++ src/main/java/by/siarheyeu/springcourse/FirstRestApp/controllers/PeopleController.java
@@ -3,6 +3,7 @@ package by.siarheyeu.springcourse.FirstRestApp.controllers;
 import by.siarheyeu.springcourse.FirstRestApp.models.Person;
 import by.siarheyeu.springcourse.FirstRestApp.services.PeopleService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,10 @@ public class PeopleController {
     @GetMapping()
     public List<Person> getPeople(){
         return peopleService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Person getPerson(@PathVariable("id") int id){
+        return peopleService.findOne(id);
     }
 }
