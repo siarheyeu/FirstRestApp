4a398b0d68f08621ba8fa60688cff53b433e9c30
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-03-21 19:38:45 +0300

    New annotations for Person and postmapping

--- src/main/java/by/siarheyeu/springcourse/FirstRestApp/controllers/PeopleController.java
+++ src/main/java/by/siarheyeu/springcourse/FirstRestApp/controllers/PeopleController.java
@@ -4,8 +4,11 @@ import by.siarheyeu.springcourse.FirstRestApp.models.Person;
 import by.siarheyeu.springcourse.FirstRestApp.services.PeopleService;
 import by.siarheyeu.springcourse.FirstRestApp.util.PersonErrorResponse;
 import by.siarheyeu.springcourse.FirstRestApp.util.PersonNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,6 +32,24 @@ public class PeopleController {
     @GetMapping("/{id}")
     public Person getPerson(@PathVariable("id") int id){
         return peopleService.findOne(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<HttpStatus> create(@RequestBody @Valid Person person,
+                                             BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            StringBuilder errorMsg = new StringBuilder();
+
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for(FieldError error : errors){
+                errorMsg.append(error.getField())
+                        .append(" - ").append(error.getDefaultMessage())
+                        .append(";");
+            }
+        }
+        peopleService.save(person);
+
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 
     @ExceptionHandler
--- src/main/java/by/siarheyeu/springcourse/FirstRestApp/models/Person.java
+++ src/main/java/by/siarheyeu/springcourse/FirstRestApp/models/Person.java
@@ -1,6 +1,9 @@
 package by.siarheyeu.springcourse.FirstRestApp.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "Person")
@@ -11,12 +14,16 @@ public class Person {
     private int id;
 
     @Column(name = "name")
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, message = "Name should be between 2 and 30 characters")
     private String name;
 
     @Column(name = "age")
+    @Min(value = 0, message = "Age should be greater than 0")
     private int age;
 
     @Column(name = "email")
+    @NotEmpty(message = "Email should not be empty")
     private String email;
 
     public Person() {
